9ae10a5b27a4e2af10ef0905592cff62c4e846ce
ganuke <gauke@gmail.com>
2024-01-17 05:13:59 +0800

    remove the completion block from production code

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -21,7 +21,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (client, sut) = makeSUT(url: url)
         
-        sut.load()
+        sut.load{_ in }
         
         XCTAssertEqual(client.requestedURLs, [url])
     }
@@ -30,8 +30,8 @@ class RemoteFeedLoaderTests: XCTestCase {
         let url = URL(string: "https://a-given-url.com")!
         let (client, sut) = makeSUT(url: url)
         
-        sut.load()
-        sut.load()
+        sut.load{_ in }
+        sut.load{_ in }
         
         XCTAssertEqual(client.requestedURLs.count, 2)
         XCTAssertEqual(client.requestedURLs, [url, url])
