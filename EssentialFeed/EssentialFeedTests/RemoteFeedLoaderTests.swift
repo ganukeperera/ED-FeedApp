2de85727f7fd589342eccae5d5aa0bde80710b1f
ganuke <gauke@gmail.com>
2024-01-17 05:05:12 +0800

    replace stubbing values by spying instead

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -39,10 +39,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_loadWithError_deliverAnErrorOnClientCall() {
         let (client, sut) = makeSUT()
-        client.error =  NSError(domain: "Test", code: 0)
         var capturedErrors = [RemoteFeedLoader.Error]()
         
         sut.load { capturedErrors.append($0) }
+        let error =  NSError(domain: "Test", code: 0)
+        client.completions[0](error)
         
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
@@ -56,13 +57,11 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-        var error: Error?
+        var completions = [(Error) -> Void]()
         
-        func get(from url: URL, completion: (Error) -> Void) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestedURLs.append(url)
-            if let error {
-                completion(error)
-            }
+            completions.append(completion)
         }
     }
 }
