1ce46a64eadb9f2232c7dea529bae1eab7219e26
ganuke <ganuke@gmail.com>
2024-01-27 18:40:00 +0800

    resume datatask oncreation

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTest.swift
@@ -17,7 +17,7 @@ class URLSessionHTTPClient {
     }
     
     func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
-        session.dataTask(with: URLRequest(url: url), completionHandler: {_, _, _ in})
+        session.dataTask(with: URLRequest(url: url), completionHandler: {_, _, _ in}).resume()
     }
 }
 
@@ -33,14 +33,42 @@ class URLSessionHTTPClientTest: XCTestCase {
         XCTAssertEqual(session.receivedURLs, [url])
     }
     
+    func test_getFromURL_resumeADataTaskWithURL() {
+        let url = URL(string: "https://a-url-session.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url: url, dataTask: task)
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url, completion: {_ in})
+        
+        XCTAssertEqual(task.resumeCount, 1)
+    }
+    
     class URLSessionSpy: URLSession {
         var receivedURLs = [URL]()
+        var stubs = [URL: URLSessionDataTask]()
+        
+        func stub(url: URL, dataTask: URLSessionDataTask) {
+            stubs[url] = dataTask
+        }
         
         override func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
             receivedURLs.append(request.url!)
-            return FakeURLSessionDataTask()
+            
+            return stubs[request.url!] ?? FakeURLSessionDataTask()
         }
     }
     
-    class FakeURLSessionDataTask: URLSessionDataTask {}
+    class FakeURLSessionDataTask: URLSessionDataTask {
+        override func resume() {
+        }
+    }
+    
+    class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCount = 0
+        override func resume() {
+            resumeCount += 1
+        }
+    }
 }
